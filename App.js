ee3cccde9c0ce5615b84ce1db0449a7e4cb04bbf
AgustinM12 <agustinmazza12345@gmail.com>
2024-04-22 13:42:10 -0300

    FEAT: se creo un proyecto de expo con tailwind

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { withExpoSnack } from 'nativewind';
 
-export default function App() {
+import { Text, View } from 'react-native';
+import { styled } from 'nativewind';
+
+const StyledView = styled(View)
+const StyledText = styled(Text)
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <StyledView className="flex-1 items-center justify-center">
+      <StyledText className="text-red-500 font-bold">
+        Try editing me! 🎉
+      </StyledText>
+
+
+      <StyledText className="text-blue-500 font-bold">
+        ou chit
+      </StyledText>
+
+    </StyledView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default withExpoSnack(App);
